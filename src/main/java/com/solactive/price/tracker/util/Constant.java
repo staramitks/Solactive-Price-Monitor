6a4982fada1fd3d5ef1cf54a39fa0c222d84ed3b
Amit Singh <amit.singh2@msci.com>
2021-05-23 11:25:33 +0530

    Update Time

--- src/main/java/com/solactive/price/tracker/util/Constant.java
+++ src/main/java/com/solactive/price/tracker/util/Constant.java
@@ -2,5 +2,5 @@ package com.solactive.price.tracker.util;
 
 public final class Constant {
 
-    public final static Integer TIME_LIMIT_SECS=600;
+    public final static Integer TIME_LIMIT_SECS=60;
 }
